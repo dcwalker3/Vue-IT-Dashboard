a417967d78f88449bd2634030afcd0ee0026acb3
Dakota Walker <dwalker29@wvstateu.edu>
2022-03-28 22:03:16 -0400

    Improved Sidebar appearance

--- src/components/SiteSidebar.vue
+++ src/components/SiteSidebar.vue
@@ -1,15 +1,19 @@
 <template>
     <div id="SideBar">
-      <div v-for="site in sites" :key="site._id">
-        <div><a :href="site.url">{{site.url}}</a>
-          <span v-if="site.currentStatus === 'Online'">
-            <i class="fa fa-circle" aria-hidden="true" style="color:green"> ✓</i>
-          </span>
-          <span v-else-if="site.currentStatus === 'Offline'">
-            <i class="fa fa-circle" aria-hidden="true" style="color:red">x</i>
-          </span>
-        </div>
+      <div id="ToolBar">
+
       </div>
+      <table id="SiteStatusTable">
+        <tr>
+          <th>URL</th>
+          <th>Status</th>
+        </tr>
+        <tr v-for="site in sites" :key="site._id">
+          <td>{{ site.url }}</td>
+          <td>{{site.currentStatus}}</td>
+        </tr>
+      </table>
+
     </div>
 </template>
 
@@ -44,16 +48,53 @@
     #SideBar {
         position: relative;
         display: flow;
-        width: fit-content;
+        width: 15%;
         height: fit-content;
         left: 0;
         border: 1px solid black;
+        border-radius: 5px;
         align-items: center;
         padding: 10px;
+        font-family: Ariel, sans-serif;
+        background-color: rgba(241, 236, 236, 0.47);
     }
 
     #SideBar a{
         text-decoration: none;
-        color: blue;
+        color: inherit;
     }
+
+    #SiteStatusTable {
+        width: 100%;
+        border-collapse: collapse;
+        border-spacing: 0;
+        border: 1px solid black;
+        border-radius: 5px;
+        font-family: Ariel, sans-serif;
+    }
+
+    #SiteStatusTable th {
+        padding: 10px;
+        border-bottom: 1px solid white;
+        border-top: 1px solid white;
+        border-right: 1px solid white;
+        border-left: 1px solid white;
+        color: white;
+        background-color: rgb(0, 0, 0);
+    }
+
+    #SiteStatusTable td {
+        padding: 10px;
+        border-bottom: 1px solid black;
+        border-top: 1px solid black;
+        border-right: 1px solid black;
+        border-left: 1px solid black;
+        background-color: rgba(241, 236, 236, 0.47);
+    }
+
+    #SiteStatusTable tr:hover {
+        background-color: rgba(164, 157, 157, 0.47);
+      cursor: pointer;
+    }
+
 </style>
